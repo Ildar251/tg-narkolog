2da1a2cb9fa2753bae6c8f8d47ea999d70553eea
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-03-25 16:54:20 +0300

    new

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
+const {Bot} = require('grammy')
 
-const TelegramApi = require('node-telegram-bot-api')
+const bot = new Bot(process.env.BOT_API_KEY);
 
-const token = '5319638653:AAE1IEol5Eugaydsk8fZxgc0NWyfrpmORpY'
-
-
-const bot = new TelegramApi(token, {polling: true})
-
-bot.on('message', async msg => {
-    const first_name = msg.from.first_name
-    const text = msg.text
-    const chatId = msg.chat.id 
-
-    await bot.sendMessage(chatId, `Привет, ${first_name}`)
+bot.command('start', async (ctx) => {
+    await ctx.reply(`Привет!`)
 })
+
+bot.on('massage', async (ctx) => {
+    await ctx.reply('Надо подумать')
+})
+
+bot.start();
