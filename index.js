07229f2b64bc820c6cf1c54dcf3f98ce27b6fa68
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-03-28 09:26:57 +0300

    grammy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard, session, InputFile } = require('grammy')
 const { Router } = require("@grammyjs/router");
+const sharp = require('sharp');
+
+// DaDate
+const DaDate_url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
 
 const { google } = require('googleapis');
 const keys = require('./winter-jet-375911-dd7563b7f443.json');
@@ -48,7 +52,7 @@ const bot = new Bot(process.env.BOT_API_KEY);
 const router = new Router((ctx) => ctx.session.step);
 
 // Use session.
-bot.use(session({ initial: () => ({ step: "", data: {} }) }));
+bot.use(session({ initial: () => ({ step: 'default', data: {} }) }));
 
 
 bot.api.setMyCommands([
@@ -77,17 +81,97 @@ ask_inn.on("message:text", async (ctx) => {
     // Проверяем, что введенный ИНН содержит только цифры
     if (/^\d+$/.test(inn)) {
 
-        // Записываем ИНН в объект сессии
-        ctx.session.data.inn = inn;
-        await ctx.reply(`🎉 ИНН успешно заполнен: ${ctx.session.data.inn} `);
+        if (ctx.message.text.length === 10 || inn.length === 12) {
 
-        ctx.session.step = 'ask_fio';
-        await ctx.reply(`Введите ФИО: 📝`);
+            const options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + process.env.DADATE_API_KEY
+                },
+                body: JSON.stringify({ query: inn })
+            }
 
+            const innKeyboard = new InlineKeyboard()
+                .text('Да ✅', `yes`)
+                .text('Нет, ввести заново', `no`);
 
+            ctx.session.data = {
+                inn: '',
+                organization: '',
+                fio: '',
+                email: '',
+                phone: '',
+                coupon: '',
+            };
+
+            ctx.session.data.inn = inn;
+
+            try {
+                const response = await fetch(DaDate_url, options);
+                const result = await response.json();
+                const organization = result.suggestions[0].value;
+                ctx.session.organization = organization;
+                await ctx.reply(`Ваша организация ${organization} ?`, {
+                    reply_markup: innKeyboard
+                });
+
+            } catch (error) {
+                await ctx.reply(`Организация не найдена, продолжить с введенным ИНН?`, {
+                    reply_markup: innKeyboard
+                });
+
+            }
+        } else {
+            await ctx.reply(`ИНН должен содержать 10 или 12 символов. Пожалуйста, введите корректный ИНН. 🚫`);
+        }
     } else {
         await ctx.reply('ИНН должен содержать только цифры. Пожалуйста, введите корректный ИНН. 🚫');
     }
+})
+
+// Обработка inline кнопок "yes" и "no"
+bot.callbackQuery('yes', async (ctx) => {
+    await ctx.answerCallbackQuery();
+    await ctx.reply(`🎉 ИНН успешно заполнен: ${ctx.session.data.inn}`);
+    ctx.session.data.organization = ctx.session.organization;
+    ctx.session.step = 'ask_email';
+    await ctx.reply(`Введите Email: 📝`);
+});
+
+bot.callbackQuery('no', async (ctx) => {
+    await ctx.answerCallbackQuery();
+    // Если пользователь отказался от введенного ИНН, переходим к началу
+    ctx.session.step = 'ask_inn';
+    await ctx.reply(`Введите ИНН: 💼`);
+});
+
+
+const ask_email = router.route("ask_email");
+
+ask_email.on("message:text", async (ctx) => {
+    const email = ctx.message.text; // Получаем введенный ФИО
+    // Записываем ФИО в объект сессии
+    ctx.session.data.email = email;
+    await ctx.reply(`🌟 Email успешно заполнен:  ${ctx.session.data.email}`);
+    ctx.session.step = 'ask_phone';
+    await ctx.reply(`Введите Телефон: 📝`);
+})
+
+
+const ask_phone = router.route("ask_phone");
+
+ask_phone.on("message:text", async (ctx) => {
+    const phone = ctx.message.text; // Получаем введенный ФИО
+
+    // Записываем ФИО в объект сессии
+    ctx.session.data.phone = phone;
+    await ctx.reply(`🌟 Телефон успешно заполнен:  ${ctx.session.data.phone}`);
+
+    ctx.session.step = 'ask_fio';
+    await ctx.reply(`Введите ФИО: 📝`);
 })
 
 
@@ -101,13 +185,13 @@ ask_fio.on("message:text", async (ctx) => {
     await ctx.reply(`🌟 ФИО успешно заполнен:  ${ctx.session.data.fio}`);
 
     ctx.session.step = 'coupon';
-
     // Генерируем уникальный идентификатор купона
     const couponId = generateCouponId();
 
     const couponKeyboard = new InlineKeyboard()
         .text('5000 рублей 💸', `coupon-1`)
-        .text('15000 рубелй 💸', `coupon-2`);
+        .text('15000 рублей 💸', `coupon-2`).row()
+        .text('Выбрать оба купона 💸', `coupon-3`);
 
     ctx.session.data.couponId = couponId;
 
@@ -115,15 +199,13 @@ ask_fio.on("message:text", async (ctx) => {
     await ctx.reply(`Давайте выберем купон 🎟️`, {
         reply_markup: couponKeyboard
     });
-
-
-
 })
 
 // Метод для генерации уникального идентификатора купона
 function generateCouponId() {
     return Math.random().toString(36).substring(7);
 }
+
 
 
 // Обработка выбора купона
@@ -135,22 +217,50 @@ router.route("coupon", async (ctx) => {
     // Записываем выбранный купон в объект сессии
     ctx.session.data.coupon = coupon;
 
-    if(coupon == 'coupon-1'  ) {
-        await ctx.replyWithPhoto(new InputFile("./images/coupon5000.png"));
-    } else if(coupon == 'coupon-2') {
-        await ctx.replyWithPhoto(new InputFile("./images/coupon15000.png"));
+    const roundedCorners = Buffer.from(
+        `<svg width="575" height="302"><text x="50%" y="50%" font-size="40" text-anchot="middle">${ctx.session.data.couponId}</text></svg>`
+    );
+
+    if (coupon == 'coupon-1') {
+        const sharpImg =
+            sharp('./images/coupon5000.png')
+                .composite([{
+                    input: roundedCorners,
+                }]);
+        await ctx.replyWithPhoto(new InputFile(sharpImg));
+    } else if (coupon == 'coupon-2') {
+        const sharpImg2 =
+            sharp('./images/coupon15000.png')
+                .composite([{
+                    input: roundedCorners,
+                }]);
+        await ctx.replyWithPhoto(new InputFile(sharpImg2));
+    } else if (coupon == 'coupon-3') {
+
+        const sharpImg =
+            sharp('./images/coupon5000.png')
+                .composite([{
+                    input: roundedCorners,
+                }]);
+        const sharpImg2 =
+            sharp('./images/coupon15000.png')
+                .composite([{
+                    input: roundedCorners,
+                }]);
+        await ctx.replyWithPhoto(new InputFile(sharpImg));
+        await ctx.replyWithPhoto(new InputFile(sharpImg2));
     }
 
     ctx.session.step = 'final';
 
     const finalKeyboard = new InlineKeyboard()
-        .text('Отправить данные в таблицу 📊✉️📥', `push`).row()
+        .text('Отправить данные в таблицу 📊📥', `push`).row()
         .text('Отменить и заполнить заново 🔄📝', `again`);
 
 
-    await ctx.reply(`Данные заполнены ✅. \n \n 💼 ИНН: ${ctx.session.data.inn} \n 📝 ФИО : ${ctx.session.data.fio} \n 🎟️ Купон: ${ctx.session.data.coupon}`, {
+    await ctx.reply(`Данные заполнены ✅ \n \n 💼 ИНН: ${ctx.session.data.inn} \n 📝 ФИО : ${ctx.session.data.fio} \n 🎟️ Купон: ${ctx.session.data.coupon}`, {
         reply_markup: finalKeyboard
-    } );
+    });
 });
 
 
@@ -159,6 +269,7 @@ bot.callbackQuery('push', async (ctx) => {
     await ctx.answerCallbackQuery();
     await writeToGoogleSheet(ctx.session.data);
     await ctx.reply('Данные успешно отправлены в таблицу! 🚀📊');
+    ctx.session.step = 'default';
 });
 
 bot.callbackQuery('again', async (ctx) => {
@@ -167,6 +278,17 @@ bot.callbackQuery('again', async (ctx) => {
     ctx.session.data = {};
     ctx.session.step = 'ask_inn';
     await ctx.reply('Данные сброшены. Пожалуйста, введите ИНН заново. 🔄🔢');
+});
+
+
+const defaultRoute = router.route("default");
+
+defaultRoute.on("message:text", async (ctx) => {
+    // Проверяем, является ли сообщение командой
+    if (!ctx.message.isCommand && ctx.session.step !== 'ask_inn' && ctx.session.step !== 'ask_fio' && ctx.session.step !== 'coupon') {
+        // Если сообщение не является командой и не ожидается ввод данных, отправляем сообщение "Я вас не понимаю"
+        await ctx.reply("Я вас не понимаю.");
+    }
 });
 
 
