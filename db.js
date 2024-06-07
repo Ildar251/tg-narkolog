30329b2ac58fcb2f84295b3083082a9a7b4cea5f
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-06-07 14:40:17 +0300

    db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://ildar251:JfEw7DmUTouhns16@cluster0.8o77zwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+require('dotenv').config();
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -15,7 +15,7 @@ async function connectToDatabase() {
     try {
         await client.connect();
         console.log("Connected to MongoDB!");
-        const db = client.db("narkologKrasnodar"); 
+        const db = client.db(process.env.DB); 
         return db;
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
